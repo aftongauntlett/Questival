3b876fe4d3fd7124ce793d9e10dd571b3ae982f3
Afton <59119773+aftongauntlett@users.noreply.github.com>
2020-05-15 15:36:01 -0400

    fixing seed errors

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,3 @@
---need hard coded data to support the planned displayed data
-
---USER TABLE inserts
 INSERT INTO user (attendee_name)
 VALUES ("Jane Jamout");
 INSERT INTO user (attendee_name)
@@ -22,7 +19,6 @@ VALUES ("Anton Anime");
 INSERT INTO user (attendee_name)
 VALUES ("Frankie Foxears");
 
---EVENT TABLE data
 INSERT INTO event (event_name, event_desc, event_location, event_date)
 VALUES ("MAGFest", "Annual festival in the DC Metro area celebrating video games, and video game music, as well as surrounding and adjacent culture", "National Harbor, MD", "Jan 2021" );
 INSERT INTO event (event_name, event_desc, event_location, event_date)
@@ -36,27 +32,25 @@ VALUES ("PAX East", "A series of gaming culture festivals involving tabletop, ar
 INSERT INTO event (event_name, event_desc, event_location, event_date)
 VALUES ("Dragon Con", "The larget multi-media popculture convetion focusing on science fiction & fantasy in literature, gaming, art, music and film.", "Atlanta, GA", "No date 2021");
 
-
---INTEREST TABLE data
-INSERT INTO interest (event_name, event_desc)
+INSERT INTO interest (interest, interest_desc)
 VALUES ("Anime", "Japanese comic book and video cartoon animation characterized by colorful graphics, vibrant characters and fantastical themes." );
-INSERT INTO interest (event_name, event_desc)
+INSERT INTO interest (interest, interest_desc)
 VALUES ("Tabletop Games", "References playing games on a tabletop surface using boards, dice, markers and measures to support complex story telling through cunning decision, and complex strategy");
-INSERT INTO interest (event_name, event_desc)
+INSERT INTO interest (interest, interest_desc)
 VALUES ("LAN", "A gathering of people with computers or compatible consoles, where a LAN connection is established for the purpose of supporting multiplayer videogame playing" );
-INSERT INTO interest (event_name, event_desc)
+INSERT INTO interest (interest, interest_desc)
 VALUES ("Arcade", "Often coin-operated entertainment console machines often installed in public for the purpose of providing a game interface which can be electronic or mechanical or both.");
-INSERT INTO interest (event_name, event_desc)
+INSERT INTO interest (interest, interest_desc)
 VALUES ("Cosplay", "A portmanteu of the words 'costume' and 'play' is used to describe an art form which incorporates fashion, accessories and performance in celebration of popular characters");
-INSERT INTO interest (event_name, event_desc)
+INSERT INTO interest (interest, interest_desc)
 VALUES ("Karaoke", "A form of entertainment involving the use of recorded music playback often with a video component to display lyrics, and microphone/sound system amplification which allows a person to sing to popular songs in performance environment");
-INSERT INTO interest (event_name, event_desc)
+INSERT INTO interest (interest, interest_desc)
 VALUES ("Jamspace", "A community curated stage experience with open mic and instruments provided by collaborative live musicians" );
-INSERT INTO interest (event_name, event_desc)
-VALUES ("Console Gaming", "Annual festival in the DC Metro area celebrating video games, and video game music, as well as surrounding and adjacent culture", "National Harbor, MD", "Jan 2021" );
-INSERT INTO interest (event_name, event_desc)
+INSERT INTO interest (interest, interest_desc)
+VALUES ("Console Gaming", "Annual festival in the DC Metro area celebrating video games, and video game music, as well as surrounding and adjacent culture" );
+INSERT INTO interest (interest, interest_desc)
 VALUES ("Panel Discussions", "Live or virtual discussion amongst a selected group who share differeing perspectives on a common theme often related to some form of popular media." );
-INSERT INTO interest (event_name, event_desc)
+INSERT INTO interest (interest, interest_desc)
 VALUES ("Video Games", "A fully interactive, electronic simulation of story-telling, action and adventure, quest, competition or puzzle solving (or a combination of all these things) incremented on by inputs and outputs." );
-INSERT INTO interest (event_name, event_desc)
+INSERT INTO interest (interest, interest_desc)
 VALUES ("Literature", "Written and spoken word material often sub-grouped by genre which includes techniques such as poetry, drama, fiction, journalism or song." );
